440da2aa9ee9cf52ba51665372ae105ead7fb8db
joey mizrahi <joeymizrahi42@gmail.com>
2021-01-11 00:41:28 +0200

    added each of the files for classes. added UML. added globals (color and window) add requirements.txt

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # Pong
 simple pong to learn to work with git 
+
+
+To run, open this project and then in terminal:
+"pip install -r requirements.txt"
+
+This will install all the needed dependencies for this game.
---
+++ globals.py
@@ -0,0 +1,25 @@
+"""
+This file is for global variables needed throughout the game
+"""
+import pygame
+
+from enum import Enum
+class Color(Enum):
+    BLUE = (0, 0, 128)
+    GREEN = (0, 200, 0)
+    WHITE = (255, 255, 255)
+    SHADOW = (192, 192, 192)
+    LIGHTBLUE = (0, 0, 255)
+    RED = (200, 0, 0)
+    LIGHTRED = (255, 100, 100)
+    PURPLE = (102, 0, 102)
+    LIGHTPURPLE = (153, 0, 153)
+
+
+class Window:
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+        self.window = pygame.display.set_mode((width,height))
+
+main_window = Window(400, 300)
