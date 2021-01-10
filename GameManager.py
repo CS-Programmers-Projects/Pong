b038269f378da2d4c983992d9097366e4ef9ca1a
joey mizrahi <joeymizrahi42@gmail.com>
2021-01-11 00:50:26 +0200

    started implementation of game manager

---
+++ GameManager.py
@@ -0,0 +1,13 @@
+# from Ball import Ball
+# from Paddle import Paddle
+# from AutoPaddle import AutoPaddle
+# from ScoreBoard import ScoreBoard
+class Game():
+    def __init__(self):
+
+
+    # Draws the arena the game will be played in.
+    def draw(self):
+
+
+    def update(self):
