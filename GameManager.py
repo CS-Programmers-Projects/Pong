13ea1a6dccf717bccb552af0503482659303f555
joey mizrahi <joeymizrahi42@gmail.com>
2021-01-11 15:10:55 +0200

    implemented layout of gameManager

--- GameManager.py
+++ GameManager.py
@@ -2,12 +2,35 @@
 # from Paddle import Paddle
 # from AutoPaddle import AutoPaddle
 # from ScoreBoard import ScoreBoard
-class Game():
+from globals import *
+import pygame
+class GameManager():
     def __init__(self):
+        # self.ball = Ball(x,y,widt,height,speed)
+        # self.player_paddle = Paddle(x,y,widt,height)
+        # self.bot_paddle = AutoPaddle(x,y,widt,height, self.ball, speed)
+        # self.scoreboard =  ScoreBoard()
+        pass
 
-
-    # Draws the arena the game will be played in.
     def draw(self):
+        main_window.window.fill((0,0,0))
+        pygame.draw.rect(main_window.window, Color.WHITE,
+                         ((0,0),(main_window.width,main_window.height)),20)
+
+        pygame.draw.line(main_window.window, Color.WHITE,
+                         (int(main_window.width // 2), 0),
+                         (int(main_window.width / 2), main_window.height),
+                         int(10/ 4))
+
+        # self.ball.draw()
+        # self.scoreboard.draw()
+        # self.player_paddle.draw()
+        # self.bot_paddle.draw()
 
 
     def update(self):
+        # self.ball.move()
+        # self.bot_paddle.move()
+        # TODO, logic of ball passing or hitting paddles...
+        pass
+
