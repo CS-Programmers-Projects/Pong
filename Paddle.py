708956c089ab3ebbfb502a1fbf8c247707ff9148
Michal <mkhakshoor13@gmail.com>
2021-01-11 23:56:52 +0200

    init commit

---
+++ Paddle.py
@@ -0,0 +1,20 @@
+from globals import *
+import pygame
+
+class Paddle():
+    def __init__(self, x, w, h, speed):
+        self.x = x
+        self.y = Window.height/2 - h/2
+        self.width = w
+        self.height = h
+        self.speed = speed
+
+    def draw(self):
+        pygame.draw.rect(main_window.window, Color.WHITE, (self.x, self.y, self.width, self.height))
+
+    def move(self):
+        keys= pygame.key.get_pressed()
+        if keys[pygame.K_UP]:
+            self.y += self.speed
+        if keys[pygame.K_DOWN]:
+            self.y -= self.speed
