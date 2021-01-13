1d29a9e4b6abc221227f4ee80fb7937213bdf827
msj1995 <1995msj1995@gmail.com>
2021-01-13 13:22:17 +0200

    Innit AutoPAddle implemented draw implemented move implemented child class - innit

--- GameManager.py
+++ GameManager.py
@@ -4,6 +4,7 @@ from Paddle import Paddle
 # from ScoreBoard import ScoreBoard
 from globals import *
 import pygame
+from AutoPaddle import AutoPaddle
 
 
 class GameManager():
@@ -11,23 +12,24 @@ class GameManager():
         # self.ball = Ball(x,y,width,height,speed)
         self.player_paddle = Paddle(20, 10, 50, 5)
         # self.bot_paddle = AutoPaddle(x,y,width,height, self.ball, speed)
+        self.bot_paddle = AutoPaddle(370, 10, 50, 0, 5)
+
         # self.scoreboard =  ScoreBoard()
         pass
 
     def draw(self):
-
         main_window.window.fill(Color.BLACK.value)
         pygame.draw.rect(main_window.window, Color.WHITE.value,
                          ((0, 0), (main_window.width, main_window.height)), 20)
 
-        pygame.draw.line(main_window.window,  Color.WHITE.value,
+        pygame.draw.line(main_window.window, Color.WHITE.value,
                          (int(main_window.width // 2), 0),
-                         (int(main_window.width / 2), main_window.height),int(10 / 4))
+                         (int(main_window.width / 2), main_window.height), int(10 / 4))
 
         # self.ball.draw()
         # self.scoreboard.draw()
         self.player_paddle.draw()
-        # self.bot_paddle.draw()
+        self.bot_paddle.draw()
 
     def update(self):
         # self.ball.move()
