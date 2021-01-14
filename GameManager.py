98de6ec77243b988f91dbad45f53bf927d3cc9d0
joey mizrahi <joeymizrahi42@gmail.com>
2021-01-14 22:32:59 +0200

    removed the line in middle of the game window. called the ball logic

--- GameManager.py
+++ GameManager.py
@@ -1,6 +1,5 @@
-# from Ball import Ball
+from Ball import Ball
 from Paddle import Paddle
-# from AutoPaddle import AutoPaddle
 from ScoreBoard import ScoreBoard
 from globals import *
 import pygame
@@ -9,33 +8,31 @@ from AutoPaddle import AutoPaddle
 
 class GameManager():
     def __init__(self):
-        # self.ball = Ball(x,y,width,height,speed)
+        self.ball = Ball(int(main_window.width // 2), int(main_window.height // 2), 7, 2)
         self.player_paddle = Paddle(20, 10, 50, 5)
-        # self.bot_paddle = AutoPaddle(x,y,width,height, self.ball, speed)
-
-        
         self.bot_paddle = AutoPaddle(370, 10, 50, 0, 5)
-        self.scoreboard =  ScoreBoard()
-
-
+        self.scoreboard = ScoreBoard()
 
     def draw(self):
         main_window.window.fill(Color.BLACK.value)
         pygame.draw.rect(main_window.window, Color.WHITE.value,
                          ((0, 0), (main_window.width, main_window.height)), 20)
 
-        pygame.draw.line(main_window.window, Color.WHITE.value,
-                         (int(main_window.width // 2), 0),
-                         (int(main_window.width / 2), main_window.height), int(10 / 4))
-
-        # self.ball.draw()
+        self.ball.draw()
         self.scoreboard.draw()
         self.player_paddle.draw()
         self.bot_paddle.draw()
 
     def update(self):
-        # self.ball.move()
-        # self.bot_paddle.move()
+        self.ball.move()
+        self.bot_paddle.move()
         self.player_paddle.move()
-        # TODO, logic of ball passing or hitting paddles...
-        pass
+
+        if self.ball.hit_paddle(self.bot_paddle):
+            self.ball.bounce("x")
+        elif self.ball.hit_paddle(self.player_paddle):
+            self.ball.bounce("x")
+        elif self.ball.pass_player():
+            print("Passed player")
+        elif self.ball.pass_computer():
+            print("passed bot")
