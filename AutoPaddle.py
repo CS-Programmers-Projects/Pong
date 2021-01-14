fd4e7929f2adb74045423fd847e986cc7f0c09e1
msj1995 <1995msj1995@gmail.com>
2021-01-14 23:45:59 +0200

    removed draw changed move mechanics

--- AutoPaddle.py
+++ AutoPaddle.py
@@ -5,14 +5,12 @@ import pygame
 class AutoPaddle(Paddle):
 
     def __init__(self, x, w, h, ball, speed):
-        self.ball = ball
         super().__init__(x, w, h, speed)
-
-    def draw(self):
-        pygame.draw.rect(main_window.window, Color.WHITE.value, (self.x, self.y, self.width, self.height))
+        self.ball = ball
 
     def move(self):
-        if self.ball.y > self.y:
+
+        if self.ball.y > self.rect.center:
             self.y += self.speed
-        if self.ball.y < self.y:
+        if self.ball.y < self.rect.center:
             self.y += self.speed
