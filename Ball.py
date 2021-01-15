709fd1f062535a926806b53927ca317c81d5499d
Tomerfi1210 <tomerfi1210@gmail.com>
2021-01-15 12:24:42 +0200

    added hit functions for the wall and paddles check if ball cross the the paddles

--- Ball.py
+++ Ball.py
@@ -1,68 +1,67 @@
 """class ball by TOMER"""
 
-from pygame import sprite, draw
+from pygame import sprite, draw, Rect
 from globals import *
 from Paddle import Paddle
 import math
+from random import randint
 
 
 class Ball(sprite.Sprite):
-    def __init__(self, x: int, y: int, radius: int, speed: float):
+    def __init__(self, x: int, y: int, width: int, height: int, speed: float):
         super(Ball, self).__init__()
         self.__start_x = x
         self.__start_y = y
-        self.speed_x = speed
-        self.speed_y = speed
-        self.x = x
-        self.y = y
-        self.radius = radius
+        self.speed = speed
+        self.dir_x = speed
+        self.dir_y = speed
+        self.rect = Rect(x, y, width, height)
+        self.radius = height/2
 
     def draw(self):
-        draw.circle(main_window.window, Color.RED.value, [self.x, self.y], self.radius)
+        draw.circle(main_window.window, Color.RED.value, self.rect.center, self.radius)
 
     def move(self):
-        tmp_x = self.x + self.speed_x
-        tmp_y = self.y + self.speed_y
-
-        if self.pass_player():
-            self.speed_x = -self.speed_x
-        elif tmp_y < 10 + self.radius:  # ceiling
+        if self.hit_floor():
             self.bounce('y')
-        elif tmp_y > main_window.height - 10 - self.radius:  # floor
+        elif self.hit_ceiling():
             self.bounce('y')
-        elif tmp_x + self.radius > main_window.width - 10:
+        elif self.pass_computer():
             self.bounce('x')
         else:
-            self.x = tmp_x
-            self.y = tmp_y
+            self.rect.x += self.dir_x
+            self.rect.y += self.dir_y
+            #self.rect.move(self.x + self.dir_x, self.y + self.dir_y)
+            # self.x += self.dir_x
+            # self.y += self.dir_y
 
-    def hit_paddle(self, paddle) -> bool:
-        return math.sqrt((self.x - paddle.x) ** 2 + (self.y - paddle) ** 2) + self.radius < 10
+    def hit_paddle(self, paddle: Paddle) -> bool:
+        return self.rect.colliderect(paddle.rect)
 
     def hit_floor(self):
-        tmp_y = self.y + self.speed_y
-        if tmp_y > main_window.height - 10 - self.radius:
-            self.bounce('y')
+        return self.rect.y + self.dir_y + self.radius > main_window.height - 10
 
     def hit_ceiling(self):
-        tmp_y = self.y + self.speed_y
-        if tmp_y > main_window.height - 10 - self.radius:
-            self.bounce('x')
+        tmp_y = self.rect.y + self.dir_y
+        return tmp_y < 10 + self.radius
 
     def pass_computer(self) -> bool:
-        tmp_x = self.x + self.speed_x
+        tmp_x = self.rect.x + self.dir_x
         return tmp_x + self.radius > main_window.width - 10
 
     def pass_player(self) -> bool:
-        tmp_x = self.x + self.speed_x
+        tmp_x = self.rect.x + self.dir_x
         return tmp_x < 10 + self.radius
 
     def bounce(self, axis: str) -> None:
         if axis.lower() == 'x':
-            self.speed_x *= -1
+            self.dir_x *= -1
         else:
-            self.speed_y *= -1
+            self.dir_y *= -1
 
     def reset_pos(self):
-        self.x = self.__start_x
-        self.y = self.__start_y
+        self.rect.x = self.__start_x
+        self.rect.y = self.__start_y
+
+        self.dir_x = 6
+        self.dir_y = 6
