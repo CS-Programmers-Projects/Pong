2af7e989106fc72de2519021a3db741cb2b11a28
Tomerfi1210 <tomerfi1210@gmail.com>
2021-01-15 12:27:36 +0200

    add rect object to paddle

--- Paddle.py
+++ Paddle.py
@@ -4,13 +4,14 @@ import pygame
 class Paddle(pygame.sprite.Sprite):
     def __init__(self, x, w, h, speed):
         self.x = x
-        self.y = main_window.height/2 - h/2
+        self.y = main_window.height / 2 - h / 2
         self.width = w
         self.height = h
         self.speed = speed
+        self.rect = pygame.Rect(x, self.y, w, h)
 
     def draw(self):
-        pygame.draw.rect(main_window.window,  Color.WHITE.value, (self.x, self.y, self.width, self.height))
+        pygame.draw.rect(main_window.window, Color.WHITE.value, (self.x, self.y, self.width, self.height))
 
     def move(self):
         keys = pygame.key.get_pressed()
